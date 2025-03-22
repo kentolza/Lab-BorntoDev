6cf3652b7d0790d915d714c4c52bfb7dbcf2fa16
Tewasil <51691094+kentolza@users.noreply.github.com>
2025-03-22 10:21:10 +0700

    new func sub1 to log time currnt

--- lab1.js
+++ lab1.js
@@ -1,6 +1,16 @@
+
 <script>
+  const date = new Date();
+
+  function sub1(prm) {
+    console.log(`this time is.. ${date}`)
+  }
+
   function begin_func() {
     console.log('Hello')
+    sub1()
   }
   begin_func();
+
+
 </script>
